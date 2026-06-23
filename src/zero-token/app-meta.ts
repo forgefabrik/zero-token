fbef653538e806341003b455611d667417679c92
forgefabrik <v.soleil.j@gmail.com>
2026-06-24 01:02:02 +0200

    refactor(core): centralize Nova application metadata

---
+++ src/zero-token/app-meta.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = "Nova";
+export const APP_ID = "nova";
+export const LEGACY_APP_NAME = "Zero Token";
+export const APP_VERSION = "0.2.0";
+export const DEFAULT_GATEWAY_URL = "http://127.0.0.1:3000/v1";
+export const YOYO_SOURCE_URL = "https://github.com/yologdev/yoyo-evolve";
