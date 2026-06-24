cccd474675620a99b627b751234ef116241328f1
forgefabrik <v.soleil.j@gmail.com>
2026-06-24 13:14:36 +0200

    deploy: start Chromium with noVNC and internal CDP

---
+++ deploy/bkg/remote-browser/entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -eu
+
+WIDTH="${NOVA_BROWSER_WIDTH:-1280}"
+HEIGHT="${NOVA_BROWSER_HEIGHT:-900}"
+DISPLAY_NUMBER="${DISPLAY#:}"
+
+rm -f "/tmp/.X${DISPLAY_NUMBER}-lock"
+rm -rf "/tmp/.X11-unix/X${DISPLAY_NUMBER}"
+mkdir -p /data/chromium
+
+Xvfb "$DISPLAY" -screen 0 "${WIDTH}x${HEIGHT}x24" -ac +extension RANDR &
+openbox-session >/tmp/openbox.log 2>&1 &
+x11vnc \
+  -display "$DISPLAY" \
+  -forever \
+  -shared \
+  -nopw \
+  -listen 0.0.0.0 \
+  -rfbport 5900 \
+  >/tmp/x11vnc.log 2>&1 &
+websockify \
+  --web=/usr/share/novnc/ \
+  0.0.0.0:6080 \
+  127.0.0.1:5900 \
+  >/tmp/websockify.log 2>&1 &
+
+exec chromium \
+  --no-sandbox \
+  --disable-dev-shm-usage \
+  --disable-gpu \
+  --remote-debugging-address=0.0.0.0 \
+  --remote-debugging-port=9222 \
+  --remote-allow-origins=* \
+  --user-data-dir=/data/chromium \
+  --window-size="${WIDTH},${HEIGHT}" \
+  --start-maximized \
+  about:blank
