0c14995a3fc7b10b141fe1381e9d0ce05a2dd4c3
forgefabrik <v.soleil.j@gmail.com>
2026-06-24 12:34:01 +0200

    fix(web): enable remote login tab flow

--- web-console/src/main.ts
+++ web-console/src/main.ts
@@ -1,5 +1,8 @@
 import App from "./App.svelte";
 import { mount } from "svelte";
+import { installRemoteLoginInterceptor } from "./lib/remote-login-interceptor";
+
+installRemoteLoginInterceptor();
 
 const app = mount(App, {
   target: document.getElementById("app")!,
